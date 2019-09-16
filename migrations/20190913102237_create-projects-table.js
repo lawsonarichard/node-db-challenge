08112693169912bc1605e5d6f174a9e1875ba52b
Richard Lawson <lawson.a.richard@gmail.com>
2019-09-16 00:49:17 -0700

    Update 20190913102237_create-projects-table.js
    updated projects table

--- migrations/20190913102237_create-projects-table.js
+++ migrations/20190913102237_create-projects-table.js
@@ -2,15 +2,15 @@ exports.up = function(knex) {
   return knex.schemea
     .createTable("projects", tbl => {
       tbl.increments();
-      tbl.string("name", 128).notNullable();
-      tbl.string("description", 128);
-      tbl.boolean("completed").notNullable();
+      tbl.string("project_name", 128).notNullable();
+      tbl.string("project_description", 128);
+      tbl.boolean("completed", false).notNullable();
     })
     .createTable("tasks", tbl => {
       tbl.increments();
-      tbl.string("description").notNullable;
-      tbl.string("notes");
-      tbl.boolean("completed");
+      tbl.string("task_description").notNullable;
+      tbl.string("task_notes");
+      tbl.boolean("completed", false).notNullable();
     })
     .createTable("project_resources", tbl => {
       tbl.increments();
@@ -19,11 +19,22 @@ exports.up = function(knex) {
         .unsigned.notNullable()
         .reference("id")
         .inTable("projects");
+      onDelete("CASCADE").onUpdate("CASECADE");
       tbl
         .integer("resources_id")
         .unsigned.notNullable()
         .reference("id")
         .inTable("resources");
+      .onDelete("CASCADE").onUpdate("CASECADE");
+    })
+
+    .createTable("resources", tbl => {
+      tbl.increments();
+      tbl
+        .string("resource_name")
+        .notNullable()
+        .unique();
+      table.string("resource_descirption");
     });
 };
 
